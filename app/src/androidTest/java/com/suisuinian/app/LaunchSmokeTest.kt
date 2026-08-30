463e257019c2788b37db0c553c7f9795fd85b955
qxdnzbl-arch <qxdnzbl@gmail.com>
2026-08-30 22:53:22 +0800

    Fix smoke test to use Espresso

--- app/src/androidTest/java/com/suisuinian/app/LaunchSmokeTest.kt
+++ app/src/androidTest/java/com/suisuinian/app/LaunchSmokeTest.kt
@@ -1,8 +1,10 @@
 package com.suisuinian.app
 
-import androidx.compose.ui.test.assertExists
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithText
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
@@ -11,11 +13,10 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class LaunchSmokeTest {
     @get:Rule
-    val composeRule = createAndroidComposeRule<SocialExperimentActivity>()
+    val activityRule = ActivityScenarioRule(SocialExperimentActivity::class.java)
 
     @Test
     fun freshInstall_launchesWithoutCrash_andShowsLogin() {
-        composeRule.onNodeWithText("登录", useUnmergedTree = true).assertExists()
-        composeRule.onNodeWithText("进入你的消息和朋友", useUnmergedTree = true).assertExists()
+        onView(withText("进入你的消息和朋友")).check(matches(isDisplayed()))
     }
 }
