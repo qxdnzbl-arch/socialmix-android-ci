167bdd54f34bfdf141b917d573ca3b7d22a749f3
qxdnzbl-arch <qxdnzbl@gmail.com>
2026-09-01 17:22:42 +0800

    Keep library acceptance aligned after recent removal

--- app/src/androidTest/java/com/immersive/music/Phase1AcceptanceTest.kt
+++ app/src/androidTest/java/com/immersive/music/Phase1AcceptanceTest.kt
@@ -96,7 +96,6 @@ class Phase1AcceptanceTest {
         composeRule.onAllNodesWithText("最近播放").assertCountEquals(0)
         composeRule.onNodeWithContentDescription("打开我喜欢的音乐").assertIsDisplayed().performClick()
         composeRule.onNodeWithText("我喜欢的音乐").assertIsDisplayed()
-        composeRule.onNodeWithContentDescription("返回音乐库").performClick()
 
         composeRule.onNodeWithContentDescription("更多:First Light").performClick()
         composeRule.onNodeWithText("歌曲选项").assertIsDisplayed()
