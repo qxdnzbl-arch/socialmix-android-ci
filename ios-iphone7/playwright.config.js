9437e538baef46fe789ce8daaa54d6a0b664a8a0
qxdnzbl-arch <qxdnzbl@gmail.com>
2026-09-06 12:53:55 +0800

    Configure iPhone 7 WebKit acceptance

---
+++ ios-iphone7/playwright.config.js
@@ -0,0 +1,1 @@
+const {defineConfig,devices}=require('@playwright/test');module.exports=defineConfig({testDir:'./tests',timeout:30000,retries:0,use:{baseURL:'http://127.0.0.1:4174',...devices['iPhone 7'],browserName:'webkit'},webServer:{command:'npx http-server . -p 4174 -c-1',port:4174,reuseExistingServer:false}});
