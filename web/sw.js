a8d0a3c0137999474d6db6ffdf02ecb49c1f0fb6
qxdnzbl-arch <qxdnzbl@gmail.com>
2026-09-21 20:46:24 +0800

    Cache Kehua app icon in PWA shell

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,5 @@
 const CACHE='kehua-shell-v1';
-const SHELL=['/','/index.html','/styles.css','/app.js','/manifest.webmanifest'];
+const SHELL=['/','/index.html','/styles.css','/app.js','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
